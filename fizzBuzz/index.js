9af962c1c407b45a0248ef5ab7b667c2f85313f6
Ernesto Venencio <zayrus@gmail.com>
2021-03-22 10:38:35 -0300

    update

--- fizzBuzz/index.js
+++ fizzBuzz/index.js
@@ -1,36 +1,16 @@
-const arrayOfNumbers = [1,2,3,4,5,10,13,15,21,23,25,45,47, 1115,2003,2004]
+const n = 148
+const FIZZBUZZ = 'FizzBuzz'
+const FIZZ = 'Fizz'
+const BUZZ = 'Buzz'
 
-const len = arrayOfNumbers.length
-const fizz = 3
-const buzz = 5
-const all = fizz * buzz
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    let fizzMod = i % 3 == 0
+    let buzzMod = i % 5 == 0
+    const result = fizzMod ? buzzMod ? FIZZBUZZ : FIZZ : buzzMod ? BUZZ : i
 
-const outArray = arrayOfNumbers.map( (n) => {
-    return fizzBuzz(n)
-})
-console.log(outArray)
-
-function fizzBuzz(number) {
-    if (module(number, all)) {
-        return 'FizzBuzz'
-    } else if ( module(number, fizz)|| contain(number, fizz)) {
-        return 'Fizz'
-    } else if ( module(number, buzz) || contain(number, buzz)) {
-        return 'Buzz'
-    } else {
-        return number.toString()
-    }
+    console.log(result)
+  }
 }
-
-function module(number, module) {
-    return number % module == 0 
-}
-
-function contain(number, content) {
-    const hasNumber = content.toString()
-    return number.toString().indexOf(hasNumber) > -1
-}
-
-
 
 
